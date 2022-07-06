51b60686c12b40647b8f7ab2c68cba4e92a4cd8e
sam <samwrighthwk@gmail.com>
2022-07-06 22:04:00 +0200

    Multiple state handlers

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,26 +1,20 @@
 import {useState} from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
-
-const Button = ({text,onClick}) => <button onClick={onClick}>{text}</button>
-
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-  const increaseByOne = () => setCounter(counter+1)
-
-  const setToZero = () => setCounter(0)
-
-  const decreaseByOne = () => setCounter(counter-1)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button text="plus" onClick={increaseByOne} />
-      <Button text="zero" onClick={setToZero} />
-      <Button text="minus" onClick={decreaseByOne} />
+      {left}
+      <button onClick={() => setLeft(left + 1)}>
+        left
+      </button>
+      <button onClick={() => setRight(right + 1)}>
+        right
+      </button>
+      {right}
     </div>
   )
 }
-
 export default App
