a67e09dd83d7fa956495ef26e7990626a029c599
sam <samwrighthwk@gmail.com>
2022-07-06 21:52:45 +0200

    Passing state

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,17 +1,35 @@
 import {useState} from 'react'
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
+  const increaseByOne = () => {
+    setCounter(counter+1)
+  }
+
+  const setToZero = () => {
+    setCounter(0)
+  }
+
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
-        plus
-      </button>
-      <button onClick={() => setCounter(0)}>
-        zero
-      </button>
+      <Display counter={counter} />
+      <Button text="plus" onClick={increaseByOne} />
+      <Button text="zero" onClick={setToZero} />
     </div>
   )
 }
