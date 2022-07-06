334cc05affb209382af4fea718158b0e3ed31189
sam <samwrighthwk@gmail.com>
2022-07-06 21:59:27 +0200

    Refactored to reduce size of components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,35 +1,24 @@
 import {useState} from 'react'
 
-const Display = (props) => {
-  return (
-    <div>{props.counter}</div>
-  )
-}
+const Display = ({counter}) => <div>{counter}</div>
 
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
-  )
-}
+const Button = ({text,onClick}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  const increaseByOne = () => {
-    setCounter(counter+1)
-  }
+  const increaseByOne = () => setCounter(counter+1)
 
-  const setToZero = () => {
-    setCounter(0)
-  }
+  const setToZero = () => setCounter(0)
+
+  const decreaseByOne = () => setCounter(counter-1)
 
   return (
     <div>
       <Display counter={counter} />
       <Button text="plus" onClick={increaseByOne} />
       <Button text="zero" onClick={setToZero} />
+      <Button text="minus" onClick={decreaseByOne} />
     </div>
   )
 }
