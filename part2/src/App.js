43aa4b421bb21a51d9e626a8088954c0458dbaac
sam <samwrighthwk@gmail.com>
2022-07-07 15:06:09 +0200

    Starting part 2 exercises

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,15 +1,31 @@
-import Note from "./components/Note"
+import Header from "./components/Header"
+import Content from "./components/Content"
+import Total from "./components/Total"
 
-const App = ({notes}) => {
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [{
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+    ]
+  }
+
   return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-    </div>
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
   )
 }
 
