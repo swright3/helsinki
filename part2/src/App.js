3cded364b7262d5b67c4462db113e40c15cd9f97
sam <samwrighthwk@gmail.com>
2022-07-07 18:36:28 +0200

    Added note filter

--- part2/src/App.js
+++ part2/src/App.js
@@ -4,6 +4,7 @@ import Note from './components/Note'
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(false)
 
   const addNote = (event) => {
     event.preventDefault()
@@ -22,11 +23,17 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
+  console.log(notesToShow)
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
