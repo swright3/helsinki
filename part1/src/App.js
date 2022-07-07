1c3cf3350b5adebd7b7a79b16bdee924282d659b
sam <samwrighthwk@gmail.com>
2022-07-07 12:16:58 +0200

    Ex1.12

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,19 +2,19 @@ import { useState } from 'react'
 
 const Button = ({text,onClick}) => <button onClick={onClick}>{text}</button>
 
-const Stat = ({text,value}) => <div>{text} {value}</div>
+const Stat = ({text,value}) => <tr><td>{text}</td><td>{value}</td></tr>
 
 const Statistics = ({good,bad,neutral}) => {
   if (good+bad+neutral === 0) {return <div>No feedback given</div>}
   return (
-    <>
+    <table><tbody>
       <Stat text="good" value={good} />
       <Stat text="neutral" value={neutral} />
       <Stat text="bad" value={bad} />
       <Stat text="all" value={good+neutral+bad} />
       <Stat text="average" value={(good-bad)/(good+neutral+bad)} />
       <Stat text="positive" value={`${(good*100)/(good+neutral+bad)}%`} />
-    </>
+      </tbody></table>
   )
 }
 
@@ -23,12 +23,23 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [selected, setSelected] = useState("Press button to be enlightened")
 
   const handleGoodClick = () => setGood(good+1)
 
   const handleNeutralClick = () => setNeutral(neutral+1)
 
   const handleBadClick = () => setBad(bad+1)
+
+  const anecdotes = [
+    "benis sus",
+    "I don't have any convictions",
+    "When I die, just throw me in the trash",
+    "I'm not superstitious, but I am a little stitious",
+    "Well, well, well. How the turntables"
+  ]
+
+  const newAnecdote = () => setSelected(anecdotes[Math.floor(Math.random()*anecdotes.length)])
 
   return (
     <div>
@@ -38,6 +49,8 @@ const App = () => {
       <Button text="bad" onClick={handleBadClick} />
       <h1>Statistics</h1>
       <Statistics good={good} bad={bad} neutral={neutral} />
+      <Button text="Beep boop" onClick={newAnecdote} />
+      <div>{selected}</div>
     </div>
   )
 }
