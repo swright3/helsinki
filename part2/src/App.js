dae53ba83e69e1ab584e1a94144e52ecb6fdea86
sam <samwrighthwk@gmail.com>
2022-07-10 17:28:07 +0200

    Ex2.9

--- part2/src/App.js
+++ part2/src/App.js
@@ -3,13 +3,17 @@ import Number from './components/Number'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "0800-00-1066" }
+    { name: 'Arto Hellas', number: "0800-00-1066" },
+    { name: 'Danny Devito', number: "80085" },
+    { name: 'Charlie Kelly', number: "6R33N-M4N" }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setNewFilter(event.target.value)
 
   const checkName = () => {
     if (persons.some((person) => person.name === newName)) {
@@ -37,6 +41,8 @@ const App = () => {
     setNewNumber('')
   }
 
+  const namesToShow = persons.filter((person) => person.name.toLowerCase().includes(newFilter))
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -53,8 +59,10 @@ const App = () => {
       </form>
       <div>debug: {newName}</div>
       <div>debug: {newNumber}</div>
+      <div>debug: {newFilter}</div>
       <h2>Numbers</h2>
-      {persons.map((person) => <Number key={person.name} name={person.name} number={person.number} />)}
+      <h3>Filter: <input value={newFilter} onChange={handleFilterChange}/></h3>
+      {namesToShow.map((person) => <Number key={person.name} name={person.name} number={person.number} />)}
     </div>
   )
 }
