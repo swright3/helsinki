1cfb7144e03592669f37973d1ad19ae36c435422
sam <samwrighthwk@gmail.com>
2022-07-11 00:27:08 +0200

    Added folder for notes app

---
+++ notes/src/App.js
@@ -0,0 +1,52 @@
+import {useState} from 'react'
+import Note from './components/Note'
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(false)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const newNotes = [...notes]
+    newNotes.push({
+      id: notes.length+1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random()<0.5
+    })
+    setNotes(newNotes)
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+          <input 
+            value={newNote}
+            onChange={handleNoteChange}
+          />
+          <button type="submit">save</button>
+      </form>
+      <button onClick={() => setShowAll(!showAll)}>
+        show/hide favourites
+      </button>
+    </div>
+  )
+}
+
+export default App
